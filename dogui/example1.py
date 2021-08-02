c6aae0c4201218185daceb2a976a1989eb0cbf52
Dominik Vach <vach.dominik@gmail.com>
2021-08-03 00:43:16 +0200

    examples

--- dogui/example1.py
+++ dogui/example1.py
@@ -1,9 +1,12 @@
 
-from dogui_core import * 
+import dogui.dogui_core as dg
 
 
+def do_nothing():
+    pass
 
-gui1=GUI()
+
+gui1=dg.GUI()
 gui1.add_menu("Menu",['Function1','Function2'],[do_nothing,do_nothing])
 
 gui1.add_menu("Menu2",['Function3','Function4'],[do_nothing,do_nothing])
